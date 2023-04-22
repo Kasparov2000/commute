ba01fa2ff7af82e346f369417139413378df8c2a
Kasparov2000 <t.mhangami@alustudent.com>
2023-04-22 11:02:08 +0200

    adding other files

---
+++ app/page.js
@@ -0,0 +1,12 @@
+'use client'
+
+import Image from 'next/image'
+import {Box, Heading} from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Heading>
+      Tafara
+    </Heading>
+  )
+}
